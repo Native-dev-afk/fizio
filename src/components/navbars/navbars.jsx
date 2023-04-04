9b2d450a77ccdedc87ce246e01fb687556764a61
Sardor <84078673+Sardo0r@users.noreply.github.com>
2023-04-05 02:09:18 +0500

    build fix error for netlify Button was imported but never used it just annoying me ;(

--- src/components/navbars/navbars.jsx
+++ src/components/navbars/navbars.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Innologo from "../../assets/imgs/innologo.png";
 import "./navbars.scss";
--- src/components/scrollButton/scrollButton.jsx
+++ src/components/scrollButton/scrollButton.jsx
@@ -1,21 +1,21 @@
-import { Button } from 'bootstrap';
 import './scrollButton.scss'
 
 
 
 function ScrollButton() {
-    return ( 
+    return (
         <>
             <div className="button-fixed">
-                <div onClick={()=> window.scrollTo({
-                top:0,
-                behavior: "smooth",})}>
+                <div onClick={() => window.scrollTo({
+                    top: 0,
+                    behavior: "smooth",
+                })}>
                     <i class="fas fa-angle-up"></i>
                 </div>
             </div>
-            
+
         </>
-     );
+    );
 }
 
 export default ScrollButton;
